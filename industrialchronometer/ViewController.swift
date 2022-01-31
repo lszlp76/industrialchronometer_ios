48ff7e9f0863036995afef2e7b3c41569a062352
zlp <zlpatwork@gmail.com>
2022-01-31 23:21:50 +0300

    Initial Commit

---
+++ industrialchronometer/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  industrialchronometer
+//
+//  Created by ulas özalp on 31.01.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
